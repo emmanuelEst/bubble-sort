9fa9be693c0ae1c9079f8e0e1442bf26604f699d
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-10-21 12:48:06 -0500

    Add code for bubble_sort method
    The code is not the most elegant solution, but I hope to learn more
    about how to refactor it later
    
    This method is destructive, meaning the original array is modified

--- main.rb
+++ main.rb
@@ -1,3 +1,23 @@
-def bubble_sort(array); end
+def bubble_sort(array)
+  i = 0
+  modified = false
 
-bubble_sort([4, 3, 78, 2, 0, 2])
+  while i <= array.length - 2
+    temp1 = array[i]
+    temp2 = array[i + 1]
+    if temp1 > temp2
+      array[i] = temp2
+      array[i + 1] = temp1
+      modified = true
+    end
+    i += 1
+  end
+
+  if modified
+    bubble_sort(array)
+  else
+    array
+  end
+end
+
+p bubble_sort([4, 3, 78, 2, 0, 2])
